088c7f6db5bbe0af6629754410b10055e0abecfe
Thilo Horstmann <thilo.horstmann@gmail.com>
2018-05-02 22:03:16 +0200

    bump version to 0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lambdacd-junit "0.3.0"
+(defproject lambdacd-junit "0.3.1-SNAPSHOT"
   :description "Junit reports in LambdaCD"
   :url "https://github.com/thilo11/lambdacd-junit"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org"}]
-                        ["snapshots" {:sign-releases false :url "https://clojars.org"}]])
+  :deploy-repositories [["releases"  {:sign-releases true :url "https://clojars.org"}]
+                        ["snapshots" {:sign-releases true :url "https://clojars.org"}]])
